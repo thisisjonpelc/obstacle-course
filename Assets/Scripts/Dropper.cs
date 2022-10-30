3f1ff6bac40ff52dd87ff4ea632395af8d5d79de
thisisjonpelc <thisisjonpelc@gmail.com>
2022-10-30 14:52:18 -0500

    Added additional functionality and components to Dropper

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+        if (Time.time > timeToWait){
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
     }
 }
